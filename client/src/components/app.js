8938ca05804a62283ea2eee4940ad7395a949449
Scott Hill <scott289@gmail.com>
2016-09-05 17:46:40 -0700

    removed demo elems

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 import PlantsView from './plants/plants-view'
-import LikeButton from './like-button'
-import Button from './button'
 import logo from '../logo.svg';
 import './app.css';
 
 
 class App extends Component {
-
-  buttonClick(value) {
-    console.log("Clicked " + value);
-  }
 
   render() {
     return (
@@ -21,11 +15,6 @@ class App extends Component {
         </div>
         <p className="App-intro">
         </p>
-        <div>
-          <Button value='Clicking me writes to console.' onClick={this.buttonClick()}/>
-          <br/><br/>
-        </div>
-        <LikeButton />
         <PlantsView />
       </div>
     );
