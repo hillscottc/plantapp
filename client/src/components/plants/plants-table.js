f81787f200dc6592b7d44b74785266e20bd34e67
Scott Hill <scott289@gmail.com>
2016-09-11 19:51:53 -0700

    clickable style

--- client/src/components/plants/plants-table.js
+++ client/src/components/plants/plants-table.js
@@ -1,5 +1,6 @@
 import React, { PropTypes }  from 'react'
 import { Table } from 'react-bootstrap';
+import './plants-table.css';
 
 /*
 This `props.columnClick.bind(this, 'symbol')` prepends `symbol` to the arg list
@@ -12,21 +13,24 @@ const PlantsTable = (props) => (
     <br/>
     <Table striped bordered condensed hover responsive>
       <thead>
-        <tr><th>symbol</th><th>synonym</th><th>family</th><th>common</th><th>sci-name</th></tr>
+        <tr><th>Symbol</th><th>Synonym</th><th>Family</th><th>Common</th><th>Sci-name</th></tr>
       </thead>
       <tbody>
         {props.plants.map((plant) =>
           <tr key={plant.id} >
-            <td onClick={props.columnClick.bind(this, 'symbol')}>
+            <td className="clickable"
+                onClick={props.columnClick.bind(this, 'symbol')}>
               {plant.symbol}
             </td>
             <td>
               {plant.synonym}
             </td>
-            <td onClick={props.columnClick.bind(this, 'family')}>
+            <td className="clickable"
+                onClick={props.columnClick.bind(this, 'family')}>
               {plant.family}
             </td>
-            <td onClick={props.columnClick.bind(this, 'common')}>
+            <td className="clickable"
+                onClick={props.columnClick.bind(this, 'common')}>
               {plant.common_name}
             </td>
             <td>{plant.sci_name}</td>
