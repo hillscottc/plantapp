8ab7776de508fc3f5de902fbe85c976e7e3c9d3f
Scott Hill <scott289@gmail.com>
2016-09-13 14:36:53 -0700

    conts

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -7,7 +7,7 @@ const Home = (props) => (
     <div>
       <div className="page-header" style={headerStyle}>
         <h1>Plants
-          <p><small>driven by data from the <a target="_blank" href="https://plants.usda.gov/dl_all.html">USDA Plants Database</a></small></p>
+          <p><small>driven by data from the <a target="_blank" href="https://plants.usda.gov/dl_all.html">USDA Plants Database</a> of 90,000+ species</small></p>
         </h1>
       </div>
       <PlantsView />
