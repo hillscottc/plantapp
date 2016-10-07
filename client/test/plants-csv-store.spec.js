c6faa367b50517c2e373f68323bec0e763e8681e
Scott Hill <scott289@gmail.com>
2016-10-07 15:33:49 -0700

    formatting

--- client/test/plants-csv-store.spec.js
+++ client/test/plants-csv-store.spec.js
@@ -7,14 +7,14 @@ import {getAll, getBySymbol, getLikeCommon} from '../src/stores/plants-csv-store
 describe("plants-csv-store test",function(){
 
 
-  it("all plants",function(done){
+  it("all plants", function(done){
     getAll((plants) => {
       plants.length.should.be.above(5);
       done();
     });
   });
 
-  it("plants by symbol",function(done){
+  it("plants by symbol", function(done){
     this.timeout(3000);
 
     getBySymbol('NAAM', (plants) => {
@@ -24,7 +24,7 @@ describe("plants-csv-store test",function(){
   });
 
 
-  it("plants like common name ",function(done){
+  it("plants like common name ", function(done){
     this.timeout(5000);
 
     getLikeCommon('yarrow', (plants) => {
