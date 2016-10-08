86d7f74a820c62a7500bfda6fb47af59332a58cc
Scott Hill <scott289@gmail.com>
2016-10-07 21:30:10 -0700

    promises!

--- client/src/stores/plants-csv-store.js
+++ client/src/stores/plants-csv-store.js
@@ -1,53 +1,66 @@
-const fs = require('fs');
-const path = require("path");
-const parse = require('csv-parse');
+import fs from 'fs';
+import path from "path";
+import parse from 'csv-parse';
 import PlantModel from './plant-model'
+import Promise from "bluebird";
 
 const dataFile = path.join(__dirname, '../../../data/plants.csv');
 
-
-export function getPlantsList(callback) {
-  const plants = [];
-  fs.createReadStream(dataFile)
-      .pipe(parse({delimiter: ','}))
-      .on('data', (csvrow) => {
-        plants.push(PlantModel.fromArray(csvrow));
-      })
-      .on('end', () => {
-        callback(plants);
-      });
+export function getPlantsList() {
+  return new Promise((resolve, reject) => {
+    const plants = [];
+    fs.createReadStream(dataFile)
+        .pipe(parse({delimiter: ','}))
+        .on('data', (csvrow) => {
+          plants.push(PlantModel.fromArray(csvrow));
+        })
+        .on('end', () => {
+          resolve(plants);
+        });
+  });
 }
 
+
 export function getPlantsBySymbol(symbol, callback) {
-  const plants = [];
-  fs.createReadStream(dataFile)
-      .pipe(parse({delimiter: ','}))
-      .on('data', (csvrow) => {
-        plants.push(PlantModel.fromArray(csvrow));
-      })
-      .on('end', () => {
-        const matches = plants.filter((plant) => {
-          return plant.symbol === symbol;
+  return new Promise((resolve, reject) => {
+    const plants = [];
+    fs.createReadStream(dataFile)
+        .pipe(parse({delimiter: ','}))
+        .on('data', (csvrow) => {
+          plants.push(PlantModel.fromArray(csvrow));
+        })
+        .on('end', () => {
+          const matches = plants.filter((plant) => {
+            return plant.symbol === symbol;
+          });
+          resolve(matches);
         });
-        callback(matches);
-      });
+  });
 }
 
 
 export function getPlantsByCommon(common, callback) {
-  const plants = [];
-  fs.createReadStream(dataFile)
-      .pipe(parse({delimiter: ','}))
-      .on('data', (csvrow) => {
-        plants.push(PlantModel.fromArray(csvrow));
-      })
-      .on('end', () => {
-        const matches = plants.filter((plant) => {
-          const re = new RegExp(common, 'i');
-          return plant.common_name.match(re);
+  return new Promise((resolve, reject) => {
+    const plants = [];
+    fs.createReadStream(dataFile)
+        .pipe(parse({delimiter: ','}))
+        .on('data', (csvrow) => {
+          plants.push(PlantModel.fromArray(csvrow));
+        })
+        .on('end', () => {
+          const matches = plants.filter((plant) => {
+            const re = new RegExp(common, 'i');
+            return plant.common_name.match(re);
+          });
+          resolve(matches);
         });
-        callback(matches);
-      });
+  });
 }
+
+
+
+
+
+
 
 
--- client/test/plants-csv-store.spec.js
+++ client/test/plants-csv-store.spec.js
@@ -1,34 +1,36 @@
 /*
 Test the plants-csv-store
  */
-import {getPlantsList, getPlantsBySymbol, getPlantsByCommon} from '../src/stores/plants-csv-store'
+import {getPlantsList, getPlantsBySymbol,
+    getPlantsListAsync, getPlantsByCommon} from '../src/stores/plants-csv-store'
 
 
 describe("plants-csv-store test",function(){
 
 
   it("all plants",function(done){
-    getPlantsList((plants) => {
+    getPlantsList().then((plants) => {
       plants.length.should.be.above(5);
       done();
-    });
+    })
   });
+
 
   it("plants by symbol",function(done){
     this.timeout(3000);
 
-    getPlantsBySymbol('NAAM', (plants) => {
+    getPlantsBySymbol('NAAM').then((plants) => {
       plants.length.should.be.above(1);
-      // console.log(plants);
       done();
-    });
+    })
+
   });
 
 
   it("plants like common name ",function(done){
     this.timeout(5000);
 
-    getPlantsByCommon('yarrow', (plants) => {
+    getPlantsByCommon('yarrow').then((plants) => {
       plants.length.should.be.above(19);
       done();
     });
