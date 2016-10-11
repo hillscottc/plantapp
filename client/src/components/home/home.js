a76a8ca56788149a55b6ed7f29a230fa9e6a7f71
Scott Hill <scott289@gmail.com>
2016-10-10 18:02:17 -0700

    header better

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -6,6 +6,8 @@ const Home = (props) => (
     <div className="homeDiv">
       <div className="page-header" id="header">
         <h1>Plants</h1>
+        <p>driven by a Postgres database built from 90,000 records of data supplied by <a target="_blank" href="https://plants.usda.gov/dl_all.html">the USDA</a>
+        </p>
       </div>
       <PlantsView />
       <br/>
