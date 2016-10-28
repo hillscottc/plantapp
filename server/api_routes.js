0a9c550bdc42af233aab999e2e8cc5555897fbbe
Scott Hill <scott289@gmail.com>
2016-10-27 21:38:24 -0700

    less tests

--- server/api_routes.js
+++ server/api_routes.js
@@ -17,21 +17,10 @@ const MAX = config.maxRecs;
 
 const Plant = require('./models/plant');
 
-// plants-bs by id
-router.get('/plants-bs/symbol/:id', (req, res) => {
-  Plant.forge({symbol: req.params.id})
-      .fetch()
-      .then((plant) => {
-        return res.json(plant.toJSON())
-      })
-      .catch((err) => {
-        return res.status(500).json({error: true, data: {message: err.message}});
-      });
-});
 
 
 // plant-bs all
-router.get('/plants-bs/', (req, res) => {
+router.get('/plants/', (req, res) => {
   Plant.forge()
       .query((qb) => {
         //qb is knex query builder
@@ -47,8 +36,9 @@ router.get('/plants-bs/', (req, res) => {
 });
 
 
-// plant-bs POST QUERY
-router.post('/plants-bs/', (req, res) => {
+// POST QUERY
+// Accepts post of search args to return plant records.
+router.post('/plants/', (req, res) => {
 
   console.log("Handling:", req.body);
 
@@ -80,118 +70,6 @@ router.post('/plants-bs/', (req, res) => {
 
 
 
-
-/*
- POST QUERY
- Accepts post of search args to return plant records.
- */
-router.post('/plants/', (req, res) => {
-
-  debug("Handling:", req.body);
-
-  let {family, common, symbol, sci, order} = req.body;
-  family = family ? "%" + family + "%" : "";
-  common = common ? "%" + common + "%" : "";
-  sci = sci ? "%" + sci + "%" : "";
-  symbol = symbol ? "%" + symbol.toUpperCase() + "%" : "";
-
-  const sql = SQL`SELECT * FROM plant WHERE 1 = 1 `;
-
-  if (symbol) {
-    sql.append(SQL` AND symbol LIKE ${symbol}`);
-  }
-  if (family) {
-    sql.append(SQL` AND family LIKE ${family}`);
-  }
-  if (common) {
-    sql.append(SQL` AND common_name LIKE ${common}`);
-  }
-  if (sci) {
-    sql.append(SQL` AND sci_name LIKE ${sci}`);
-  }
-
-  // note: dont use SQL escaping here
-  if (order) {
-    sql.append(` ORDER BY ${order}`);
-  }
-  sql.append(` LIMIT ${MAX}`);
-
-  debug(sql.sql);
-  debug(sql.values);
-
-  db.any(sql)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
-
-
-
-
-// GET - some plants
-router.get('/plants/', (req, res) => {
-  db.any(SQL`SELECT * FROM plant LIMIT ${MAX}`)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
-
-
-// GET - by id
-router.get('/plant/:id', (req, res) => {
-  db.oneOrNone(SQL`SELECT * FROM plant WHERE id = ${req.params.id}`)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
-
-
-// GET - by symbol
-router.get('/plants/symbol/:symbol', (req, res) => {
-  let {symbol} = req.params;
-  symbol = symbol.toUpperCase();
-  db.any(SQL`SELECT * FROM plant WHERE symbol = ${symbol}`)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
-
-// GET - by synonym
-router.get('/plants/synonym/:synonym', (req, res) => {
-  const sql = SQL`SELECT * FROM plant WHERE synonym = ${req.params.synonym}`;
-  db.any(sql)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
-
-
-// GET - like family
-router.get('/plants/family/:family', (req, res) => {
-  let {family} = req.params;
-  family = "%" + family + "%";
-  const sql = SQL`SELECT * FROM plant WHERE family LIKE ${family} LIMIT ${MAX}`.setName('get_family');
-  db.any(sql)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
-
-
-// GET - like common name
-router.get('/plants/common-name/:common', (req, res) => {
-  let {common} = req.params;
-  common = "%" + common + "%";
-  const sql = SQL`SELECT * FROM plant WHERE common_name LIKE ${common} LIMIT ${MAX}`.setName('get_common');
-  db.any(sql)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
-
-
-// GET - like sci name
-router.get('/plants/sci-name/:sci', (req, res) => {
-  let {sci} = req.params;
-  sci = "%" + sci + "%";
-  const sql = SQL`SELECT * FROM plant WHERE sci_name LIKE ${sci} LIMIT ${MAX}`.setName('get_sci');
-  db.any(sql)
-      .then((data) => { return res.json(data) })
-      .catch((error) => { console.error(error) })
-});
 
 
 module.exports = router;
--- server/api_routes.spec.js
+++ server/api_routes.spec.js
@@ -1,240 +1,13 @@
 const supertest = require("supertest");
 const should = require("should");
-const config = require('../config');
 const server = supertest.agent("http://localhost:3001");
-
-const MAX = config.maxRecs;
-
-// describe("Plants api GET tests",function(){
-//
-//   it("plants list",function(done){
-//
-//     server
-//         .get("/api/plants")
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           res.body.length.should.equal(MAX);
-//           should.not.exist(res.body.error);
-//           done();
-//         });
-//   });
-//
-//
-//   it("plant by id ",function(done){
-//
-//     server
-//         .get("/api/plant/1")
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           res.body.id.should.equal(1);
-//           should.not.exist(res.body.error);
-//           done();
-//         });
-//   });
-//
-//   it("plants by symbol ",function(done){
-//
-//     server
-//         .get("/api/plants/symbol/naam")
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           // console.log(res.body.length);
-//           res.body.length.should.equal(4);
-//           should.not.exist(res.body.error);
-//           done();
-//         });
-//   });
-//
-//
-//   it("plants by synonym ",function(done){
-//
-//     server
-//         .get("/api/plants/synonym/ABBAF")
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           // console.log(res.body.length);
-//           res.body.length.should.equal(1);
-//           should.not.exist(res.body.error);
-//           done();
-//         });
-//   });
-//
-//   it("plants LIKE family ",function(done){
-//
-//     server
-//         .get("/api/plants/family/Pleo")
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           // console.log(res.body.length);
-//           res.body.length.should.equal(14);
-//           should.not.exist(res.body.error);
-//           done();
-//         });
-//   });
-//
-//   it("plants like common name ",function(done){
-//
-//     server
-//         .get("/api/plants/common-name/okra")
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           // console.log(res.body.length);
-//           res.body.length.should.equal(3);
-//           should.not.exist(res.body.error);
-//           done();
-//         });
-//   });
-//
-//
-//   it("plants like sci name ",function(done){
-//
-//     server
-//         .get("/api/plants/sci-name/Abies")
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           // console.log(res.body.length);
-//           res.body.length.should.equal(38);
-//           should.not.exist(res.body.error);
-//           done();
-//         });
-//   });
-//
-// });
-//
-//
-// describe("Plants search query POST tests",function() {
-//
-//   it("blank query",function(done){
-//     const data = {};
-//     console.log("query:", data);
-//     server
-//         .post("/api/plants/")
-//         .send(data)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           res.body.length.should.equal(MAX);
-//           done();
-//         });
-//   });
-//
-//   it("order by family",function(done){
-//     const data = {order:'family'};
-//     console.log("query:", data);
-//     server
-//         .post("/api/plants/")
-//         .send(data)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res) {
-//           const firstPlant = res.body[0];
-//           firstPlant.family.should.equal("Acanthaceae");
-//           res.body.length.should.equal(MAX);
-//           done();
-//         });
-//   });
-//
-//   it("order by common_name, descending",function(done){
-//     const data = {order:'common_name DESC'};
-//     console.log("query:", data);
-//     server
-//         .post("/api/plants/")
-//         .send(data)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res) {
-//           const firstPlant = res.body[0];
-//           firstPlant.common_name.should.equal("‘ilima");
-//           res.body.length.should.equal(MAX);
-//           done();
-//         });
-//   });
-//
-//   it("family query",function(done){
-//     const data = {family:'Pleo'};
-//     console.log("query:", data);
-//     server
-//         .post("/api/plants/")
-//         .send(data)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           res.body.length.should.equal(14);
-//           done();
-//         });
-//   });
-//
-//
-//   it("okra query, order by symbol",function(done){
-//     const data = {family:'Malva', common: 'okra', order: 'symbol'};
-//     console.log("query:", data);
-//     server
-//         .post("/api/plants/")
-//         .send(data)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           const firstPlant = res.body[0];
-//           firstPlant.symbol.should.equal("ABELM");
-//           res.body.length.should.equal(3);
-//           done();
-//         });
-//   });
-//
-//   it("musk okra query",function(done){
-//     const data = {family:'Malva', common: 'musk'};
-//     console.log("query:", data);
-//     server
-//         .post("/api/plants/")
-//         .send(data)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           res.body.length.should.equal(2);
-//           done();
-//         });
-//   });
-//
-//   it("another okra query",function(done){
-//     const data = {sci:'Medik.', common: 'okra'};
-//     console.log("query:", data);
-//     server
-//         .post("/api/plants/")
-//         .send(data)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           res.body.length.should.equal(2);
-//           done();
-//         });
-//   });
-//
-// });
 
 
 describe("BOOKSHELF",function() {
 
-  it("plants-bs by symbol", function (done) {
-    server
-        .get("/api/plants-bs/symbol/ABELM")
-        .expect("Content-type", /json/)
-        .expect(200)
-        .end(function (err, res) {
-          // console.log(res.body);
-          done();
-        });
-  });
-
   it("plants-bs all", function (done) {
     server
-        .get("/api/plants-bs/")
+        .get("/api/plants/")
         .expect("Content-type", /json/)
         .expect(200)
         .end(function (err, res) {
@@ -248,7 +21,7 @@ describe("BOOKSHELF",function() {
   it("plants-bs search symbol", function (done) {
     const data = {symbol:'ABELM'};
     server
-        .post("/api/plants-bs/")
+        .post("/api/plants/")
         .send(data)
         .expect("Content-type",/json/)
         .expect(200)
@@ -265,7 +38,7 @@ describe("BOOKSHELF",function() {
   it("plants-bs musk okra query ",function(done){
     const data = {family:'Malva', common: 'musk'};
     server
-        .post("/api/plants-bs/")
+        .post("/api/plants/")
         .send(data)
         .expect("Content-type",/json/)
         .expect(200)
