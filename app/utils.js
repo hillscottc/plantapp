0b977e17da6d249aaad7c8801be56e35fddf2317
Scott Hill <scott289@gmail.com>
2017-01-11 06:49:46 -0800

    lose old stuff

--- app/utils.js
+++ app/utils.js
@@ -10,15 +10,3 @@ export function checkHttpResp(response) {
     throw error;
   }
 }
-
-export function parseQueryString(url) {
-
-  let urlParams = {};
-  url.replace(
-      new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-      function($0, $1, $2, $3) {
-        urlParams[$1] = $3;
-      }
-  );
-  return urlParams;
-}
